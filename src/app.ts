8aad571860a125fce9704fe37f968d3a2e01f405
VinniFachini <vinniebrasil@gmail.com>
2024-01-13 16:20:27 -0300

    fix: auth routes added manually

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,23 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
-import authRoutes from './routes/authRoutes';
+import { register, login } from './controllers/authController';
+import { handleImageUpload } from './middleware/uploadsMiddleware';
+
+dotenv.config();
 
 // Initialize Express application
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+
 
 // Middleware
 app.use(cors()); // Enable CORS
 app.use(bodyParser.json()); // Parse JSON bodies
 
-app.use('/auth', authRoutes);
+app.use('/login', login);
+app.use('/register', handleImageUpload, register);
 
 app.get('*', (req, res) => {
   res.status(404).json({error: {type: "Not Found!", message: "The content you are looking for was not found!"}})
